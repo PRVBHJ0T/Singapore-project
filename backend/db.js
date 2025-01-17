77ed68c5fbccfd234a8a2579899dcf51456930fa
PRVBHJ0T <prabhjot.werk@gmail.com>
2025-01-18 01:45:15 +0530

    Add backend server and routes
    - Add .gitignore file to backend
    - Update db.js and index.js
    - Add employeeRoutes and server.js

--- backend/db.js
+++ backend/db.js
@@ -1,3 +1,7 @@
+const {Pool}= require("pg");
+
+
+//postgres connection setup
 const pool = new Pool({
     user: "your_username",
     host: "localhost",
@@ -5,23 +9,5 @@ const pool = new Pool({
     password: "your_password",
     port: 5432,
   });
-  
 
-
-(async function initializeDatabase() {
-    const query = `
-      CREATE TABLE IF NOT EXISTS employees (
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(100) NOT NULL,
-        age INT NOT NULL,
-        citizenship_status VARCHAR(50) NOT NULL,
-        gross_salary NUMERIC NOT NULL
-      );
-    `;
-    try {
-      await pool.query(query);
-      console.log("Database initialized successfully.");
-    } catch (err) {
-      console.error("Error initializing database:", err);
-    }
-  })();
+  module.exports= pool;
--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { validate } = require("./middleware");
-const { EmployeeSchema } = require("./types");
-const app = express()
+const employeeRoutes = require("./routes/employeeRoutes");
 
+const app = express();
 
-const port = 3000
 app.use(bodyParser.json());
+app.use("/employees", employeeRoutes);
 
-//add a new employee
-app.post('/employees',validate(EmployeeSchema),(req,res)=>{
-    const {name , age , citizenship_status , gross_salary}= req.body;
-
-    try{
-        const result = await pool.query 
-    }
-})
-
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+module.exports = app;
